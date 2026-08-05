3911edb52c92aa4d42c98b5ea71828445ae662af
faithoxweb <faithoxweb@gmail.com>
2026-08-05 14:10:59 +0530

    Update razorpay.js

--- api/razorpay.js
+++ api/razorpay.js
@@ -4,7 +4,7 @@ const { Resend } = require('resend');
 
 // 1. Initialize Supabase (TYPO FIXED)
 const supabaseUrl = 'https://fdjcvpsqossuiljuadkk.supabase.co';
-const supabaseKey = process.env.SUPABSE_SERVICE_ROLE_KEY; 
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY; 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // 2. Initialize Resend
