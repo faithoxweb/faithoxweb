871c309e65c0cbe6640616a420042acbc7588481
faithoxweb <faithoxweb@gmail.com>
2026-08-12 00:52:48 +0530

    Update callback.js

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -78,8 +78,8 @@ export default async function handler(req, res) {
     // 4.5. AUTOMATICALLY REGISTER WEBHOOKS WITH SHOPIFY
     // =============================================================
     const webhooksToRegister = [
-      { topic: "app/uninstalled", address: "https://faithox.com/api/webhooks/shopify.js" },
-      { topic: "orders/paid", address: "https://faithox.com/api/webhooks/shopify.js" }
+      { topic: "app/uninstalled", address: "https://www.faithox.com/api/webhooks/shopify.js" },
+      { topic: "orders/paid", address: "https://www.faithox.com/api/webhooks/shopify.js" }
     ];
 
     for (const hook of webhooksToRegister) {
